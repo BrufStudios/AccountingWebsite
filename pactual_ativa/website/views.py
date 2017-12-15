494b8fe72b076b902171e12e1ee980ae4ca55db4
Will Glück <bigwillgluck@gmail.com>
2017-12-14 22:50:43 -0200

    Layout Básico

---
+++ pactual_ativa/website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'website/index.html')
+
+# def page(request):
+#     return render(request, reverse('website:page'))
